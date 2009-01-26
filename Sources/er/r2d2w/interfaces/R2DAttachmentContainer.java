a61857ba0736f5047dff48989fd8d6c31e8655ad
nullterminated <ramseygurley@gmail.com>
2009-01-26 11:18:10 +0000

    trunk folder

--- Sources/er/r2d2w/interfaces/R2DAttachmentContainer.java
+++
@@ -1,8 +0,0 @@
-package er.r2d2w.interfaces;
-
-import java.io.File;
-
-public interface R2DAttachmentContainer {
-	public void setUpload(File upload);
-	public File upload();
-}
--- WebServerResources/R2D2W.js
+++
@@ -1,9 +0,0 @@
-R2D2W = function() {
-	return {
-		object: function(o) {
-			function F() {};
-			F.prototype = o;
-			return new F();
-		}
-	};	
-}();
