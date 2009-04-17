3ff1ab48e58a904c27a06678d7386999ba31b2ff
nullterminated <ramseygurley@gmail.com>
2009-04-17 01:29:58 +0000

    Property level display component for KeyGlobalIDs

---
+++ ERR2d2w/Sources/er/r2d2w/components/R2D2WDisplayKeyGlobalID.java
@@ -0,0 +1,95 @@
+package er.r2d2w.components;
+
+import org.apache.log4j.Logger;
+
+import com.webobjects.appserver.WOComponent;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.directtoweb.D2W;
+import com.webobjects.directtoweb.InspectPageInterface;
+import com.webobjects.eocontrol.EOEnterpriseObject;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSMutableDictionary;
+
+import er.directtoweb.components.ERD2WStatelessComponent;
+import er.extensions.eof.ERXEC;
+import er.extensions.eof.ERXEOControlUtilities;
+import er.extensions.eof.ERXEOGlobalIDUtilities;
+import er.extensions.eof.ERXKeyGlobalID;
+import er.extensions.foundation.ERXStringUtilities;
+import er.extensions.localization.ERXLocalizer;
+import er.r2d2w.R2D2WDirectAction;
+import com.webobjects.appserver.WOActionResults;
+
+public class R2D2WDisplayKeyGlobalID extends ERD2WStatelessComponent {
+    /** logging support */
+    static final Logger log = Logger.getLogger(R2D2WDisplayKeyGlobalID.class);
+    
+    private static final String displayNameForEntityKey = "displayNameForEntityOfGlobalID";
+    private static final String primaryKeyStringForGlobalIDKey = "primaryKeyStringForGlobalID";
+    private static final String labelLocKey = "R2D2W.inspectGlobalID.buttonLabel";
+    private static final String titleLocKey = "R2D2W.inspectGlobalID.helpString";
+
+    public R2D2WDisplayKeyGlobalID(WOContext context) {
+        super(context);
+    }
+
+    public EOEnterpriseObject objectForGlobalID() {
+    	return ERXEOGlobalIDUtilities.fetchObjectWithGlobalID(ERXEC.newEditingContext(), globalIDFromObjectPropertyValue());
+    }
+    
+    public ERXKeyGlobalID globalIDFromObjectPropertyValue() {
+    	return (ERXKeyGlobalID)objectPropertyValue();
+    }
+    
+    public String inspectEntityLabel() {
+    	NSMutableDictionary<String, String> locDict = new NSMutableDictionary<String, String>();
+    	ERXKeyGlobalID gid = globalIDFromObjectPropertyValue();
+    	locDict.setObjectForKey(ERXEOControlUtilities.primaryKeyStringForGlobalID(gid), primaryKeyStringForGlobalIDKey);
+    	locDict.addEntriesFromDictionary(displayNameDictionary());
+    	ERXLocalizer loc = ERXLocalizer.currentLocalizer();
+    	return loc.localizedTemplateStringForKeyWithObject(labelLocKey, locDict);
+    }
+    
+    public String inspectEntityTitle() {
+    	ERXLocalizer loc = ERXLocalizer.currentLocalizer();
+    	return loc.localizedTemplateStringForKeyWithObject(titleLocKey, displayNameDictionary());
+    }
+    
+    protected NSDictionary<String, String> displayNameDictionary() {
+    	String entityName = globalIDFromObjectPropertyValue().entityName();
+    	String localizerKey = "Entity.name." + entityName;
+    	String result = ERXLocalizer.currentLocalizer().localizedStringForKey(localizerKey);
+        if(result == null) {
+            result = ERXStringUtilities.displayNameForKey(entityName);
+            result = localizedValueForKey(result, localizerKey);
+        }
+        return new NSDictionary<String, String>(result, displayNameForEntityKey);
+    }
+    
+	private String localizedValueForKey(String result, String localizerKey) {
+		String formerResult = ERXLocalizer.currentLocalizer().localizedStringForKey(result);
+		if(formerResult != null) {
+			result = formerResult;
+			log.info("Found an old-style entry: " + localizerKey +"->" + formerResult);
+		}
+		ERXLocalizer.currentLocalizer().takeValueForKey(result, localizerKey);
+		return result;
+	}
+
+	public NSDictionary<String, Object> queryDictionary() {
+		ERXKeyGlobalID gid = globalIDFromObjectPropertyValue();
+		return R2D2WDirectAction.inspectQueryDictionaryForKey(ERXEOControlUtilities.primaryKeyStringForGlobalID(gid));
+	}
+
+	public String inspectEntityAction() {
+		return "Inspect" + globalIDFromObjectPropertyValue().entityName();
+	}
+
+	public WOActionResults inspectEntity() {
+		InspectPageInterface ipi = D2W.factory().inspectPageForEntityNamed(globalIDFromObjectPropertyValue().entityName(), session());
+		ipi.setObject(objectForGlobalID());
+		ipi.setNextPage(context().page());
+		return (WOComponent)ipi;
+	}
+
+}
