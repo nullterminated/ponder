a457881db1d756f0c2c727470909e5300b9d13f9
nullterminated <ramseygurley@gmail.com>
2009-10-03 19:26:37 +0000

    A default button content component to use as default content for R2DLinkButtons.

---
+++ ERR2d2w/Sources/er/r2d2w/components/buttons/R2DDefaultButtonContent.java
@@ -0,0 +1,11 @@
+package er.r2d2w.components.buttons;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.components.ERD2WStatelessComponent;
+
+public class R2DDefaultButtonContent extends ERD2WStatelessComponent {
+    public R2DDefaultButtonContent(WOContext context) {
+        super(context);
+    }
+}
