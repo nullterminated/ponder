8d41f9333ae83094275199376cc8781feaba1cce
nullterminated <ramseygurley@gmail.com>
2009-10-03 19:37:39 +0000

    New action bar and controller buttons to replace the ERD2W ones.

---
+++ ERR2d2w/Sources/er/r2d2w/components/buttons/R2DActionBar.java
@@ -0,0 +1,11 @@
+package er.r2d2w.components.buttons;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.components.ERDActionBar;
+
+public class R2DActionBar extends ERDActionBar {
+    public R2DActionBar(WOContext context) {
+        super(context);
+    }
+}
