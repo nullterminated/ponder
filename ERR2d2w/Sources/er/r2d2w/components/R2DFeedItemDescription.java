23860a38f5ec33face8ff5ae48516e05d37f6270
nullterminated <ramseygurley@gmail.com>
2009-10-11 14:46:12 +0000

    Removed cdata wrapper component and moved wrapping into a string utility method

--- ERR2d2w/Sources/er/r2d2w/components/R2DFeedItemDescription.java
+++ ERR2d2w/Sources/er/r2d2w/components/R2DFeedItemDescription.java
@@ -1,10 +1,52 @@
 package er.r2d2w.components;
 
 import com.webobjects.appserver.WOContext;
+
 import er.directtoweb.components.ERD2WStatelessComponent;
+import er.extensions.foundation.ERXValueUtilities;
 
 public class R2DFeedItemDescription extends ERD2WStatelessComponent {
     public R2DFeedItemDescription(WOContext context) {
         super(context);
     }
+    
+    public String stringValue() {
+    	String value = (String)objectPropertyValue();
+    	boolean escapeHTML = ERXValueUtilities.booleanValueWithDefault(d2wContext().valueForKey("escapeHTML"), true);
+    	if(!escapeHTML) {
+    		//TODO Use this when CData patch is accepted
+    		//value = ERXStringUtilities.escapePCData(value);
+    		value = escapePCData(value);
+    	}
+    	return value;
+    }
+    
+    /**
+     * Escapes the given PCDATA string as CDATA.
+     * @param pcdata The string to escape
+     * @return the escaped string
+     */
+    public static String escapePCData(String pcdata) {
+    	if(pcdata == null) { return null; }
+    	
+    	int start = 0;
+    	int end = 0;
+    	String close = "]]>";
+    	String escape = "]]]]><![CDATA[>";
+
+    	StringBuffer sb = new StringBuffer("<![CDATA[");
+    	
+    	do {
+        	end = pcdata.indexOf(close, start);
+    		sb.append(pcdata.substring(start, (end==-1?pcdata.length():end)));
+    		if(end != -1) { sb.append(escape); }
+    		start = end;
+    		start += 3;
+    	} while (end != -1);
+    	
+    	sb.append(close);
+    	
+    	return sb.toString();
+    }
+
 }
