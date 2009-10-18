6df8d6fa5885821cc77596ff453493b5d1f3336a
nullterminated <ramseygurley@gmail.com>
2009-10-18 19:26:01 +0000

    Updated to use new link buttons

--- ERR2d2w/Sources/er/r2d2w/pages/R2D2WMessagePage.java
+++ ERR2d2w/Sources/er/r2d2w/pages/R2D2WMessagePage.java
@@ -1,6 +1,7 @@
 package er.r2d2w.pages;
 
 import com.webobjects.appserver.WOContext;
+import com.webobjects.foundation.NSDictionary;
 
 import er.directtoweb.pages.ERD2WMessagePage;
 
@@ -9,5 +10,9 @@ public class R2D2WMessagePage extends ERD2WMessagePage {
 	public R2D2WMessagePage(WOContext context) {
         super(context);
     }
+	
+	public NSDictionary<String, String> branch() {
+		return (NSDictionary<String, String>)d2wContext().valueForKeyPath("branch");
+	}
     
 }
