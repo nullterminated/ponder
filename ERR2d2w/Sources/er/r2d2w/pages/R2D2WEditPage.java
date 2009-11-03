c8f354f30270286a6cbf2d0f077caebbb5c2c6ef
nullterminated <ramseygurley@gmail.com>
2009-11-03 00:18:01 +0000

    Refresh the object after save. I forgot to add this back to the page when I removed the save button component.

--- ERR2d2w/Sources/er/r2d2w/pages/R2D2WEditPage.java
+++ ERR2d2w/Sources/er/r2d2w/pages/R2D2WEditPage.java
@@ -1,11 +1,26 @@
 package er.r2d2w.pages;
 
+import com.webobjects.appserver.WOComponent;
 import com.webobjects.appserver.WOContext;
+import com.webobjects.eocontrol.EOEditingContext;
 
 import er.directtoweb.pages.ERD2WInspectPage;
 
 public class R2D2WEditPage extends ERD2WInspectPage {
     public R2D2WEditPage(WOContext context) {
         super(context);
+    }
+    
+    /**
+     * Overridden from parent to refresh the saved object in the EC
+     * to get values for derived attributes.
+     */
+    public WOComponent submitAction() throws Throwable {
+    	WOComponent result = super.submitAction();
+    	if(errorMessages.count() == 0) {
+    		EOEditingContext ec = object().editingContext();
+    		ec.refreshObject(object());
+    	}
+    	return result;
     }
 }
