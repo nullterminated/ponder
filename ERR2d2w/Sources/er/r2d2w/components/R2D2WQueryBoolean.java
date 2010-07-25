939acc000672dd680328b2ef8333d0c833b0886a
nullterminated <ramseygurley@gmail.com>
2010-07-25 17:19:08 +0000

    Fix for broken query boolean component

--- ERR2d2w/Sources/er/r2d2w/components/R2D2WQueryBoolean.java
+++ ERR2d2w/Sources/er/r2d2w/components/R2D2WQueryBoolean.java
@@ -1,11 +1,9 @@
 package er.r2d2w.components;
 
 import com.webobjects.appserver.WOContext;
-import com.webobjects.foundation.NSArray;
 
 import er.directtoweb.components.bool.ERD2WCustomQueryBoolean;
 import er.extensions.foundation.ERXStringUtilities;
-import er.extensions.localization.ERXLocalizer;
 
 public class R2D2WQueryBoolean extends ERD2WCustomQueryBoolean {
 	
@@ -20,19 +18,6 @@ public class R2D2WQueryBoolean extends ERD2WCustomQueryBoolean {
 		super.reset();
 	}
 	
-    public String displayString() {
-    	NSArray<String> choicesNames = choicesNames();
-        String result;
-        int choicesIndex = queryNumbers.indexOf(item);
-        choicesIndex = choicesIndex == 0 ? 2 : choicesIndex - 1;
-        if(choicesNames == null || choicesIndex >= choicesNames.count()) {
-            result = super.displayString();
-        } else {
-        	result = (String)choicesNames.objectAtIndex(choicesIndex);
-        }
-        return ERXLocalizer.currentLocalizer().localizedStringForKeyWithDefault(result);
-    }
-
 	/**
 	 * @return the labelID
 	 */
---
+++ ERR2d2w/Sources/er/r2d2w/components/misc/R2DBooleanSelector.java
@@ -0,0 +1,10 @@
+package er.r2d2w.components.misc;
+
+import com.webobjects.appserver.WOContext;
+import er.extensions.components.ERXBooleanSelector;
+
+public class R2DBooleanSelector extends ERXBooleanSelector {
+    public R2DBooleanSelector(WOContext context) {
+        super(context);
+    }
+}
