fc3ac46e0e926f3a319648497fea5b388026278d
nullterminated <ramseygurley@gmail.com>
2010-10-10 00:44:44 +0000

    Moved common wrapper stuff into a switch component

---
+++ ERR2d2w/Sources/er/r2d2w/components/R2D2WContentWrapper.java
@@ -0,0 +1,10 @@
+package er.r2d2w.components;
+
+import com.webobjects.appserver.WOContext;
+import er.directtoweb.components.ERDCustomComponent;
+
+public class R2D2WContentWrapper extends ERDCustomComponent {
+    public R2D2WContentWrapper(WOContext context) {
+        super(context);
+    }
+}
