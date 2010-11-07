d80fa1d341d82e87bc973c620ab7b289242a8b64
nullterminated <ramseygurley@gmail.com>
2010-11-07 16:19:46 +0000

    New inspect and query form repetitions

---
+++ ERR2d2w/Sources/er/r2d2w/components/repetitions/R2DInspectFormRepetition.java
@@ -0,0 +1,11 @@
+package er.r2d2w.components.repetitions;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.components.repetitions.ERDAttributeRepetition;
+
+public class R2DInspectFormRepetition extends ERDAttributeRepetition {
+	public R2DInspectFormRepetition(WOContext context) {
+		super(context);
+	}
+}
---
+++ ERR2d2w/Sources/er/r2d2w/components/repetitions/R2DQueryFormRepetition.java
@@ -0,0 +1,11 @@
+package er.r2d2w.components.repetitions;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.components.repetitions.ERDAttributeRepetition;
+
+public class R2DQueryFormRepetition extends ERDAttributeRepetition {
+	public R2DQueryFormRepetition(WOContext context) {
+		super(context);
+	}
+}
