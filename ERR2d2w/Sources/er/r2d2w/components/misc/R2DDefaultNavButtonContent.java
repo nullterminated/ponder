01ff26385b3c9eb0109efc6d363ee1c08095572e
nullterminated <ramseygurley@gmail.com>
2010-12-19 15:15:09 +0000

    Created a proper default nav button content component and set it as the default

---
+++ ERR2d2w/Sources/er/r2d2w/components/misc/R2DDefaultNavButtonContent.java
@@ -0,0 +1,10 @@
+package er.r2d2w.components.misc;
+
+import com.webobjects.appserver.WOContext;
+import er.directtoweb.components.ERD2WStatelessComponent;
+
+public class R2DDefaultNavButtonContent extends ERD2WStatelessComponent {
+    public R2DDefaultNavButtonContent(WOContext context) {
+        super(context);
+    }
+}
