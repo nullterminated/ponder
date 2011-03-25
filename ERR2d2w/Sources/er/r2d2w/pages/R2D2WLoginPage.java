5680addc11c6ebe27bee8f123ea4926dd1d2e0c4
nullterminated <ramseygurley@gmail.com>
2011-03-25 04:58:39 +0000

    Initial login page implementation

---
+++ ERR2d2w/Sources/er/r2d2w/pages/R2D2WLoginPage.java
@@ -0,0 +1,11 @@
+package er.r2d2w.pages;
+
+import com.webobjects.appserver.WOContext;
+
+import er.directtoweb.pages.ERD2WPage;
+
+public class R2D2WLoginPage extends ERD2WPage {
+    public R2D2WLoginPage(WOContext context) {
+        super(context);
+    }
+}
