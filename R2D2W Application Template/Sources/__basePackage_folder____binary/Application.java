0cd5b759dd5573057d0da6f55f1b52783db04659
nullterminated <ramseygurley@gmail.com>
2012-01-18 21:30:22 -0700

    Moved default time zone settings into a static initializer block so they should happen before main() is called.

--- R2D2W Application Template/Sources/__basePackage_folder____binary/Application.java
+++ R2D2W Application Template/Sources/__basePackage_folder____binary/Application.java
@@ -13,30 +13,32 @@ import er.extensions.appserver.navigation.ERXNavigationManager;
 
 public class Application extends ERXApplication {
 	private static Logger log = Logger.getLogger(Application.class);
-	
-    public static void main(String argv[]) {
-        ERXApplication.main(argv, Application.class);
-    }
 
-	public void finishInitialization() {
-		//Set default time zones
-        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
-        NSTimeZone.setDefaultTimeZone(NSTimeZone.getGMT());
-
-        //Initialize the navigation manager
-        ERXNavigationManager.manager().configureNavigation();
-        
-        //Set the default request handler to the direct action request handler
-        String key = directActionRequestHandlerKey();
-        WORequestHandler handler = requestHandlerForKey(key);
-        setDefaultRequestHandler(handler);
+	static {
+		// Set default time zones
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+		NSTimeZone.setDefaultTimeZone(NSTimeZone.getGMT());
 	}
 
-    public void didFinishLaunching() {
-    	/* ** put your post initialization code in here ** */
-    	
-    	//FIXME Add the preference relationship to the actor entity
-    	//ERCoreBusinessLogic.sharedInstance().addPreferenceRelationshipToActorEntity(Person.ENTITY_NAME);
-    }
+	public static void main(String argv[]) {
+		ERXApplication.main(argv, Application.class);
+	}
+
+	public void finishInitialization() {
+		// Initialize the navigation manager
+		ERXNavigationManager.manager().configureNavigation();
+
+		// Set the default request handler to the direct action request handler
+		String key = directActionRequestHandlerKey();
+		WORequestHandler handler = requestHandlerForKey(key);
+		setDefaultRequestHandler(handler);
+	}
+
+	public void didFinishLaunching() {
+		/* ** put your post initialization code in here ** */
+		
+		// FIXME Add the preference relationship to the actor entity
+		// ERCoreBusinessLogic.sharedInstance().addPreferenceRelationshipToActorEntity(Person.ENTITY_NAME);
+	}
 
 }
