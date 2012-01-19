1a80d4bf97d978c44ea99acdd834e0b60718bb01
nullterminated <ramseygurley@gmail.com>
2012-01-18 20:47:41 -0700

    Adding svg images for inline table buttons

---
+++ ERR2d2w/Sources/er/r2d2w/components/misc/R2DSVGUseImage.java
@@ -0,0 +1,42 @@
+package er.r2d2w.components.misc;
+
+import com.webobjects.appserver.WOApplication;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WOResourceManager;
+
+import er.directtoweb.components.ERD2WStatelessCustomComponentWithArgs;
+
+public class R2DSVGUseImage extends ERD2WStatelessCustomComponentWithArgs {
+    public R2DSVGUseImage(WOContext context) {
+        super(context);
+    }
+    
+    public String href() {
+    	return (String)valueForBinding("href");
+    }
+    
+    public String filename() {
+    	return (String)valueForBinding("filename");
+    }
+    
+    public String framework() {
+    	String framework = (String)valueForBinding("framework");
+    	return framework == null?"app":framework;
+    }
+    
+    public String imageID() {
+    	return (String)valueForBinding("imageID");
+    }
+
+	public String imageHref() {
+		String url = href();
+		if(url == null && filename() != null) {
+			WOResourceManager rm = WOApplication.application().resourceManager();
+			url = rm.urlForResourceNamed(filename(), framework(), null, context().request());
+		}
+		if(url == null) {
+			url = "";
+		}
+		return url + "#" + imageID();
+	}
+}
