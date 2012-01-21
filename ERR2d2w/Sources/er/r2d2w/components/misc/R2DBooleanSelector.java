d1a4a9ca0d5ecadfc31dfa8c7016b78d1c208ec4
nullterminated <ramseygurley@gmail.com>
2012-01-21 15:58:39 -0700

    Removed unused method

--- ERR2d2w/Sources/er/r2d2w/components/misc/R2DBooleanSelector.java
+++ ERR2d2w/Sources/er/r2d2w/components/misc/R2DBooleanSelector.java
@@ -4,7 +4,6 @@ import com.webobjects.appserver.WOContext;
 
 import er.extensions.components.ERXBooleanSelector;
 import er.extensions.foundation.ERXStringUtilities;
-import er.extensions.localization.ERXLocalizer;
 
 public class R2DBooleanSelector extends ERXBooleanSelector {
 	private String labelID;
@@ -24,10 +23,5 @@ public class R2DBooleanSelector extends ERXBooleanSelector {
 	public void reset() {
 		super.reset();
 		labelID = null;
-	}
-
-	public String yesName() {
-		String yesString = ERXLocalizer.currentLocalizer().localizedStringForKeyWithDefault(stringValueForBinding("yesString", "Yes"));
-		return yesString;
 	}
 }
