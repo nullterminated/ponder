95ec341a8be8638a066ecd4a525e2ae3c0571831
nullterminated <ramseygurley@gmail.com>
2012-01-21 21:41:08 -0700

    Made R2DErrorPanel stateless

--- ERR2d2w/Sources/er/r2d2w/components/R2DErrorPanel.java
+++ ERR2d2w/Sources/er/r2d2w/components/R2DErrorPanel.java
@@ -2,9 +2,9 @@ package er.r2d2w.components;
 
 import com.webobjects.appserver.WOContext;
 
-import er.directtoweb.components.ERDCustomComponent;
+import er.directtoweb.components.ERD2WStatelessCustomComponentWithArgs;
 
-public class R2DErrorPanel extends ERDCustomComponent {
+public class R2DErrorPanel extends ERD2WStatelessCustomComponentWithArgs {
     public R2DErrorPanel(WOContext context) {
         super(context);
     }
