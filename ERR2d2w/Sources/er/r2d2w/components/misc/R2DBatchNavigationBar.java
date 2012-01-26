6fde4083acade5b15838fb69d3a3428b91f49b96
nullterminated <ramseygurley@gmail.com>
2012-01-25 22:09:16 -0700

    Re-implemented batch navigation component

--- ERR2d2w/Sources/er/r2d2w/components/misc/R2DBatchNavigationBar.java
+++ ERR2d2w/Sources/er/r2d2w/components/misc/R2DBatchNavigationBar.java
@@ -1,51 +1,158 @@
 package er.r2d2w.components.misc;
 
 import com.webobjects.appserver.WOContext;
+import com.webobjects.appserver.WODisplayGroup;
+import com.webobjects.directtoweb.D2WContext;
+import com.webobjects.foundation.NSDictionary;
+import com.webobjects.foundation.NSNotificationCenter;
+import com.webobjects.foundation.NSRange;
 
-import er.extensions.batching.ERXBatchNavigationBar;
+import er.extensions.components.ERXStatelessComponent;
+import er.extensions.eof.ERXConstant;
+import er.extensions.foundation.ERXValueUtilities;
 
-/**
- * XHTML Strict version of ERXBatchNavigationBar to page through display groups.<br />
- * 
- * @binding d2wContext the D2W context that this component is in
- * @binding displayGroup the WODisplayGroup that is being controlled
- * @binding objectName the name of the type of object that is contained in the WODisplayGroup
- * @binding sortKeyList an NSArray of sort key paths that will be displayed in a popup button
- * @binding clearSelection boolean that indicates if the selection should be reset on paging (default false)
- */
-public class R2DBatchNavigationBar extends ERXBatchNavigationBar {
-
-	private String batchSizeID;
-	private String batchIndexID;
+public class R2DBatchNavigationBar extends ERXStatelessComponent {
+	private WODisplayGroup displayGroup;
+	private transient D2WContext d2wContext;
+	private Object batchSize;
+	private NSRange middleRange;
+	private Integer index;
 
 	public R2DBatchNavigationBar(WOContext context) {
-        super(context);
-    }
+		super(context);
+	}
 
 	public void reset() {
-		batchSizeID = null;
-		batchIndexID = null;
 		super.reset();
+		displayGroup = null;
+		d2wContext = null;
+		batchSize = null;
+		middleRange = null;
+	}
+
+	public WODisplayGroup displayGroup() {
+		if (displayGroup == null) {
+			displayGroup = (WODisplayGroup) valueForBinding("displayGroup");
+		}
+		return displayGroup;
 	}
 	
-	/**
-	 * @return the batchSizeID
-	 */
-	public String batchSizeID() {
-		if(batchSizeID == null) {
-			batchSizeID = "id" + context().elementID();
+	public D2WContext d2wContext() {
+		if (d2wContext == null) {
+			d2wContext = (D2WContext) valueForBinding("d2wContext");
 		}
-		return batchSizeID;
+		return d2wContext;
 	}
 
 	/**
-	 * @return the batchIndexID
+	 * @return the batchSize
 	 */
-	public String batchIndexID() {
-		if(batchIndexID == null) {
-			batchIndexID = "id" + context().elementID();
+	public Object batchSize() {
+		return batchSize;
+	}
+
+	/**
+	 * @param batchSize
+	 *            the batchSize to set
+	 */
+	public void setBatchSize(Object batchSize) {
+		this.batchSize = batchSize;
+	}
+
+	public void selectBatchSize() {
+		int batchSize = ERXValueUtilities.intValue(batchSize());
+		displayGroup().setNumberOfObjectsPerBatch(batchSize);
+		displayGroup().setCurrentBatchIndex(1);
+		displayGroup().clearSelection();
+		NSNotificationCenter.defaultCenter().postNotification("BatchSizeChanged",
+				ERXConstant.integerForInt(batchSize),
+				new NSDictionary<String, Object>(d2wContext(), "d2wContext"));
+	}
+
+	public boolean currentBatchSizeSelected() {
+		int batchSize = ERXValueUtilities.intValue(batchSize());
+		return displayGroup().numberOfObjectsPerBatch() == batchSize;
+	}
+
+	public void selectFirstBatch() {
+		displayGroup().setCurrentBatchIndex(1);
+	}
+
+	public boolean isFirstBatchSelected() {
+		return displayGroup().currentBatchIndex() == 1;
+	}
+
+	public void selectLastBatch() {
+		displayGroup().setCurrentBatchIndex(displayGroup().batchCount());
+	}
+
+	public boolean isLastBatchSelected() {
+		return displayGroup().currentBatchIndex() == displayGroup().batchCount();
+	}
+
+	/**
+	 * @return the middleRange
+	 */
+	public NSRange middleRange() {
+		if(middleRange == null) {
+			int batchCount = displayGroup().batchCount();
+			if(batchCount == 2) {
+				middleRange = new NSRange(2,0);
+				return middleRange;
+			}
+			int currentBatch = displayGroup().currentBatchIndex();
+			int batchLimit = ERXValueUtilities.intValue(d2wContext().valueForKey("batchLimit"));
+			batchLimit = Math.max(batchLimit, 5);
+			batchLimit = Math.min(batchLimit, batchCount);
+			int centerBatch = batchLimit/2 + batchLimit%2;
+			int length = batchLimit - 2;
+			int location;
+			if(currentBatch <= centerBatch) {
+				location = 2;
+			} else if(currentBatch > batchCount - centerBatch) {
+				location = batchCount - batchLimit + 2;
+			} else {
+				location = currentBatch - centerBatch + 2;
+			}
+			middleRange = new NSRange(location, length);
 		}
-		return batchIndexID;
+		return middleRange;
+	}
+
+	public String batchSizeString() {
+		if(ERXValueUtilities.intValue(batchSize()) == 0) {
+			String all = localizer().localizedStringForKey("R2DBatchSize.all");
+			return all;
+		}
+		return String.valueOf(batchSize());
+	}
+
+	/**
+	 * @return the index
+	 */
+	public Integer index() {
+		return index;
+	}
+
+	/**
+	 * @param index the index to set
+	 */
+	public void setIndex(Integer index) {
+		this.index = index;
+	}
+
+	public String batchIndex() {
+		return String.valueOf(middleRange().location() + index().intValue());
+	}
+
+	public boolean isMiddleBatchSelected() {
+		int currentBatch = displayGroup().currentBatchIndex();
+		return currentBatch == middleRange().location() + index().intValue();
+	}
+
+	public void selectMiddleBatch() {
+		displayGroup().setCurrentBatchIndex(middleRange().location() + index().intValue());
+		middleRange = null;
 	}
 
 }
