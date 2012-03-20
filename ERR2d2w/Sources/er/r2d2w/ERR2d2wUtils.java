f1c2f14d6f075f5d8d5d77ae7259ba580cf4ca7e
nullterminated <ramseygurley@gmail.com>
2012-03-19 22:16:41 -0700

    Oops. Need to use headersForKey when determining acceptsXHTML

--- ERR2d2w/Sources/er/r2d2w/ERR2d2wUtils.java
+++ ERR2d2w/Sources/er/r2d2w/ERR2d2wUtils.java
@@ -2,6 +2,7 @@ package er.r2d2w;
 
 import com.webobjects.appserver.WORequest;
 import com.webobjects.appserver.WOResponse;
+import com.webobjects.foundation.NSArray;
 
 public class ERR2d2wUtils {
 	private static final String acceptKey = "accept";
@@ -9,8 +10,8 @@ public class ERR2d2wUtils {
 	private static final String xhtmlType = "application/xhtml+xml";
 
 	public static Boolean acceptsXHTML(WORequest request) {
-		String acceptTypes = request.headerForKey(acceptKey);
-		boolean result = (acceptTypes != null && acceptTypes.contains(xhtmlType));
+		NSArray<String> acceptTypes = request.headersForKey(acceptKey);
+		boolean result = acceptTypes.contains(xhtmlType);
 		return Boolean.valueOf(result);
 	}
 	
