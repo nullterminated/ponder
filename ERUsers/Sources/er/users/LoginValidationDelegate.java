456004ec1b1da42668a3a3a9d0d42a016f8d2418
nullterminated <ramseygurley@gmail.com>
2012-04-24 22:05:59 -0700

    Default validation delegate for login pages

---
+++ ERUsers/Sources/er/users/LoginValidationDelegate.java
@@ -0,0 +1,26 @@
+package er.users;
+
+import er.directtoweb.pages.ERD2WPage;
+import er.directtoweb.pages.ERD2WPage.ValidationDelegate;
+import er.extensions.validation.ERXValidationException;
+
+public class LoginValidationDelegate extends ValidationDelegate {
+
+	public LoginValidationDelegate(ERD2WPage page) {
+		super(page);
+	}
+
+	@Override
+	public boolean hasValidationExceptionForPropertyKey() {
+		return false;
+	}
+
+	@Override
+	public void validationFailedWithException(Throwable e, Object value, String keyPath) {
+		if (e instanceof ERXValidationException) {
+			ERXValidationException ve = (ERXValidationException)e;
+			_page.d2wContext().takeValueForKey(ve.getMessage(), "extraErrorMessage");
+		}
+	}
+
+}
