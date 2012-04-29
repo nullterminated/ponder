0605fd45a11efa4fc0239c0729653a77f16b6853
nullterminated <ramseygurley@gmail.com>
2012-04-29 16:57:44 -0700

    serialVersionUIDs

--- ERUsers/Sources/er/users/model/ERCredential.java
+++ ERUsers/Sources/er/users/model/ERCredential.java
@@ -12,6 +12,13 @@ import er.extensions.crypting.ERXCrypto;
  * can be limited.
  */
 public class ERCredential extends er.users.model.eogen._ERCredential {
+	/**
+	 * Do I need to update serialVersionUID?
+	 * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
+	 * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@SuppressWarnings("unused")
 	private static final Logger log = Logger.getLogger(ERCredential.class);
 
--- ERUsers/Sources/er/users/model/ERUser.java
+++ ERUsers/Sources/er/users/model/ERUser.java
@@ -23,6 +23,12 @@ import er.extensions.validation.ERXValidationFactory;
  * The user class.
  */
 public class ERUser extends er.users.model.eogen._ERUser implements ERCoreUserInterface {
+	/**
+	 * Do I need to update serialVersionUID?
+	 * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
+	 * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
+	 */
+	private static final long serialVersionUID = 1L;
 
 	private static final Logger log = Logger.getLogger(ERUser.class);
 
