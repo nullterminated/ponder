91fc0caf600b4dcef37fb5e7c1ca075aff27ce0f
nullterminated <ramseygurley@gmail.com>
2012-05-19 21:08:58 -0700

    Update ERCoreBL migration 0

--- ERCoreBL/Sources/er/corebl/migrations/ERCoreBL0.java
+++ ERCoreBL/Sources/er/corebl/migrations/ERCoreBL0.java
@@ -26,16 +26,55 @@ public class ERCoreBL0 extends ERXMigrationDatabase.Migration {
 
 	@Override
 	public void upgrade(EOEditingContext editingContext, ERXMigrationDatabase database) throws Throwable {
+		ERXMigrationTable ercMailClobTable = database.newTableNamed("ERCMailClob");
+		ercMailClobTable.newBigIntegerColumn("id", false);
+		ercMailClobTable.newStringColumn("message", 10000000, false);
+
+
+		ercMailClobTable.create();
+	 	ercMailClobTable.setPrimaryKey("id");
+
 		ERXMigrationTable ercMailRecipientTable = database.newTableNamed("ERCMailRecipient");
 		ercMailRecipientTable.newBigIntegerColumn("id", false);
 		ercMailRecipientTable.newBigIntegerColumn("mailAddressID", false);
 		ercMailRecipientTable.newBigIntegerColumn("mailMessageID", false);
-		ercMailRecipientTable.newStringColumn("type", 4, false);
+		ercMailRecipientTable.newStringColumn("recipientType", 3, false);
 
-		ercMailRecipientTable.addUniqueIndex("mailAddressID_mailMessageID_idx", ercMailRecipientTable.existingColumnNamed("mailMessageID"), ercMailRecipientTable.existingColumnNamed("mailAddressID"));
+		ercMailRecipientTable.addUniqueIndex("mailAddressID_mailMessageID_idx", ercMailRecipientTable.existingColumnNamed("mailAddressID"), ercMailRecipientTable.existingColumnNamed("mailMessageID"));
 
 		ercMailRecipientTable.create();
 	 	ercMailRecipientTable.setPrimaryKey("id");
+
+		ERXMigrationTable ercMailAttachmentTable = database.newTableNamed("ERCMailAttachment");
+		ercMailAttachmentTable.newIntegerColumn("attachmentID", true);
+		ercMailAttachmentTable.newStringColumn("filePath", 1000, true);
+		ercMailAttachmentTable.newBigIntegerColumn("id", false);
+		ercMailAttachmentTable.newBigIntegerColumn("mailMessageID", false);
+		ercMailAttachmentTable.newStringColumn("token", 50, false);
+
+		ercMailAttachmentTable.addUniqueIndex("attachmentID_mailMessageID_idx", ercMailAttachmentTable.existingColumnNamed("mailMessageID"), ercMailAttachmentTable.existingColumnNamed("attachmentID"));
+		ercMailAttachmentTable.addUniqueIndex("mailMessageID_token_idx", ercMailAttachmentTable.existingColumnNamed("token"), ercMailAttachmentTable.existingColumnNamed("mailMessageID"));
+
+		ercMailAttachmentTable.create();
+	 	ercMailAttachmentTable.setPrimaryKey("id");
+
+		ERXMigrationTable ercAuditTrailTable = database.newTableNamed("ERCAuditTrail");
+		ercAuditTrailTable.newStringColumn("gid", 255, false);
+		ercAuditTrailTable.newBigIntegerColumn("id", false);
+		ercAuditTrailTable.newIntBooleanColumn("isDeleted", false);
+
+		ercAuditTrailTable.addUniqueIndex("ERCAuditTrail_gid_idx", ercAuditTrailTable.existingColumnNamed("gid"));
+
+		ercAuditTrailTable.create();
+	 	ercAuditTrailTable.setPrimaryKey("id");
+
+		ERXMigrationTable ercAuditClobTable = database.newTableNamed("ERCAuditClob");
+		ercAuditClobTable.newBigIntegerColumn("id", false);
+		ercAuditClobTable.newStringColumn("valuesString", 10000000, false);
+
+
+		ercAuditClobTable.create();
+	 	ercAuditClobTable.setPrimaryKey("id");
 
 		ERXMigrationTable ercPreferenceTable = database.newTableNamed("ERCPreference");
 		ercPreferenceTable.newBigIntegerColumn("id", false);
@@ -47,47 +86,6 @@ public class ERCoreBL0 extends ERXMigrationDatabase.Migration {
 
 		ercPreferenceTable.create();
 	 	ercPreferenceTable.setPrimaryKey("id");
-
-		ERXMigrationTable ercMailClobTable = database.newTableNamed("ERCMailClob");
-		ercMailClobTable.newBigIntegerColumn("id", false);
-		ercMailClobTable.newStringColumn("message", 10000000, false);
-
-
-		ercMailClobTable.create();
-	 	ercMailClobTable.setPrimaryKey("id");
-
-		ERXMigrationTable ercMailAddressTable = database.newTableNamed("ERCMailAddress");
-		ercMailAddressTable.newStringColumn("emailAddress", 254, false);
-		ercMailAddressTable.newBigIntegerColumn("id", false);
-		ercMailAddressTable.newFlagBooleanColumn("isActive", false);
-
-		ercMailAddressTable.addUniqueIndex("ERCMailAddress_emailAddress_idx", ercMailAddressTable.existingColumnNamed("emailAddress"));
-
-		ercMailAddressTable.create();
-	 	ercMailAddressTable.setPrimaryKey("id");
-
-		ERXMigrationTable ercMailCategoryTable = database.newTableNamed("ERCMailCategory");
-		ercMailCategoryTable.newStringColumn("detail", 1000, false);
-		ercMailCategoryTable.newIntegerColumn("id", false);
-		ercMailCategoryTable.newStringColumn("name", 50, false);
-
-
-		ercMailCategoryTable.create();
-	 	ercMailCategoryTable.setPrimaryKey("id");
-
-		ERXMigrationTable ercAuditTrailEntryTable = database.newTableNamed("ERCAuditTrailEntry");
-		ercAuditTrailEntryTable.newTimestampColumn("created", false);
-		ercAuditTrailEntryTable.newBigIntegerColumn("id", false);
-		ercAuditTrailEntryTable.newStringColumn("keyPath", 100, true);
-		ercAuditTrailEntryTable.newBigIntegerColumn("newClobID", true);
-		ercAuditTrailEntryTable.newBigIntegerColumn("oldClobID", true);
-		ercAuditTrailEntryTable.newBigIntegerColumn("trailID", false);
-		ercAuditTrailEntryTable.newStringColumn("type", 50, false);
-		ercAuditTrailEntryTable.newStringColumn("userGlobalID", 255, true);
-
-
-		ercAuditTrailEntryTable.create();
-	 	ercAuditTrailEntryTable.setPrimaryKey("id");
 
 		ERXMigrationTable ercMailMessageTable = database.newTableNamed("ERCMailMessage");
 		ercMailMessageTable.newTimestampColumn("dateRead", true);
@@ -109,19 +107,6 @@ public class ERCoreBL0 extends ERXMigrationDatabase.Migration {
 		ercMailMessageTable.create();
 	 	ercMailMessageTable.setPrimaryKey("id");
 
-		ERXMigrationTable ercMailAttachmentTable = database.newTableNamed("ERCMailAttachment");
-		ercMailAttachmentTable.newIntegerColumn("attachmentID", true);
-		ercMailAttachmentTable.newStringColumn("filePath", 1000, true);
-		ercMailAttachmentTable.newBigIntegerColumn("id", false);
-		ercMailAttachmentTable.newBigIntegerColumn("mailMessageID", false);
-		ercMailAttachmentTable.newStringColumn("token", 50, false);
-
-		ercMailAttachmentTable.addUniqueIndex("attachmentID_mailMessageID_idx", ercMailAttachmentTable.existingColumnNamed("attachmentID"), ercMailAttachmentTable.existingColumnNamed("mailMessageID"));
-		ercMailAttachmentTable.addUniqueIndex("mailMessageID_token_idx", ercMailAttachmentTable.existingColumnNamed("token"), ercMailAttachmentTable.existingColumnNamed("mailMessageID"));
-
-		ercMailAttachmentTable.create();
-	 	ercMailAttachmentTable.setPrimaryKey("id");
-
 		ERXMigrationTable ercMailAddressCategoryPreferenceTable = database.newTableNamed("ERCMailAddressCategoryPreference");
 		ercMailAddressCategoryPreferenceTable.newBigIntegerColumn("ercMailAddressId", false);
 		ercMailAddressCategoryPreferenceTable.newIntegerColumn("ercMailCategoryId", false);
@@ -130,37 +115,52 @@ public class ERCoreBL0 extends ERXMigrationDatabase.Migration {
 		ercMailAddressCategoryPreferenceTable.create();
 	 	ercMailAddressCategoryPreferenceTable.setPrimaryKey("ercMailAddressId", "ercMailCategoryId");
 
-		ERXMigrationTable ercAuditTrailTable = database.newTableNamed("ERCAuditTrail");
-		ercAuditTrailTable.newStringColumn("gid", 255, false);
-		ercAuditTrailTable.newBigIntegerColumn("id", false);
-		ercAuditTrailTable.newFlagBooleanColumn("isDeleted", false);
+		ERXMigrationTable ercMailAddressTable = database.newTableNamed("ERCMailAddress");
+		ercMailAddressTable.newStringColumn("emailAddress", 254, false);
+		ercMailAddressTable.newBigIntegerColumn("id", false);
+		ercMailAddressTable.newIntBooleanColumn("isActive", false);
 
-		ercAuditTrailTable.addUniqueIndex("ERCAuditTrail_gid_idx", ercAuditTrailTable.existingColumnNamed("gid"));
+		ercMailAddressTable.addUniqueIndex("ERCMailAddress_emailAddress_idx", ercMailAddressTable.existingColumnNamed("emailAddress"));
 
-		ercAuditTrailTable.create();
-	 	ercAuditTrailTable.setPrimaryKey("id");
+		ercMailAddressTable.create();
+	 	ercMailAddressTable.setPrimaryKey("id");
 
-		ERXMigrationTable ercAuditClobTable = database.newTableNamed("ERCAuditClob");
-		ercAuditClobTable.newBigIntegerColumn("id", false);
-		ercAuditClobTable.newStringColumn("valuesString", 10000000, false);
+		ERXMigrationTable ercAuditTrailEntryTable = database.newTableNamed("ERCAuditTrailEntry");
+		ercAuditTrailEntryTable.newTimestampColumn("created", false);
+		ercAuditTrailEntryTable.newBigIntegerColumn("id", false);
+		ercAuditTrailEntryTable.newStringColumn("keyPath", 100, true);
+		ercAuditTrailEntryTable.newBigIntegerColumn("newClobID", true);
+		ercAuditTrailEntryTable.newBigIntegerColumn("oldClobID", true);
+		ercAuditTrailEntryTable.newBigIntegerColumn("trailID", false);
+		ercAuditTrailEntryTable.newStringColumn("type", 50, false);
+		ercAuditTrailEntryTable.newStringColumn("userGlobalID", 255, true);
 
 
-		ercAuditClobTable.create();
-	 	ercAuditClobTable.setPrimaryKey("id");
+		ercAuditTrailEntryTable.create();
+	 	ercAuditTrailEntryTable.setPrimaryKey("id");
+
+		ERXMigrationTable ercMailCategoryTable = database.newTableNamed("ERCMailCategory");
+		ercMailCategoryTable.newStringColumn("detail", 1000, false);
+		ercMailCategoryTable.newIntegerColumn("id", false);
+		ercMailCategoryTable.newStringColumn("name", 50, false);
+
+
+		ercMailCategoryTable.create();
+	 	ercMailCategoryTable.setPrimaryKey("id");
 
 		ercMailRecipientTable.addForeignKey("mailAddressID", "ERCMailAddress", "id");
 		ercMailRecipientTable.addForeignKey("mailMessageID", "ERCMailMessage", "id");
-		ercAuditTrailEntryTable.addForeignKey("newClobID", "ERCAuditClob", "id");
-		ercAuditTrailEntryTable.addForeignKey("oldClobID", "ERCAuditClob", "id");
-		ercAuditTrailEntryTable.addForeignKey("trailID", "ERCAuditTrail", "id");
+		ercMailAttachmentTable.addForeignKey("attachmentID", "ERAttachment", "id");
+		ercMailAttachmentTable.addForeignKey("mailMessageID", "ERCMailMessage", "id");
 		ercMailMessageTable.addForeignKey("fromAddressID", "ERCMailAddress", "id");
 		ercMailMessageTable.addForeignKey("htmlClobID", "ERCMailClob", "id");
 		ercMailMessageTable.addForeignKey("mailCategoryID", "ERCMailCategory", "id");
 		ercMailMessageTable.addForeignKey("plainClobID", "ERCMailClob", "id");
 		ercMailMessageTable.addForeignKey("replyToAddressID", "ERCMailAddress", "id");
-		ercMailAttachmentTable.addForeignKey("attachmentID", "ERAttachment", "id");
-		ercMailAttachmentTable.addForeignKey("mailMessageID", "ERCMailMessage", "id");
 		ercMailAddressCategoryPreferenceTable.addForeignKey("ercMailAddressId", "ERCMailAddress", "id");
 		ercMailAddressCategoryPreferenceTable.addForeignKey("ercMailCategoryId", "ERCMailCategory", "id");
+		ercAuditTrailEntryTable.addForeignKey("newClobID", "ERCAuditClob", "id");
+		ercAuditTrailEntryTable.addForeignKey("oldClobID", "ERCAuditClob", "id");
+		ercAuditTrailEntryTable.addForeignKey("trailID", "ERCAuditTrail", "id");
 	}
 }
