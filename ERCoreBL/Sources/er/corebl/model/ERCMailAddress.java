7cf4932417b89ee6618facbc04f7dd6aed370f25
nullterminated <ramseygurley@gmail.com>
2012-05-19 20:46:59 -0700

    Add validation for ERCMailAddress email string.

--- ERCoreBL/Sources/er/corebl/model/ERCMailAddress.java
+++ ERCoreBL/Sources/er/corebl/model/ERCMailAddress.java
@@ -4,6 +4,10 @@ import org.apache.log4j.Logger;
 
 import com.webobjects.eocontrol.EOEditingContext;
 
+import er.extensions.net.ERXEmailValidator;
+import er.extensions.validation.ERXValidationException;
+import er.extensions.validation.ERXValidationFactory;
+
 public class ERCMailAddress extends er.corebl.model.eogen._ERCMailAddress {
 	/**
 	 * Do I need to update serialVersionUID? See section 5.6 <cite>Type Changes
@@ -21,6 +25,24 @@ public class ERCMailAddress extends er.corebl.model.eogen._ERCMailAddress {
 	public static class ERCMailAddressClazz<T extends ERCMailAddress> extends
 			er.corebl.model.eogen._ERCMailAddress._ERCMailAddressClazz<T> {
 		/* more clazz methods here */
+		
+		protected ERXEmailValidator emailValidator;
+		
+		public ERXEmailValidator emailValidator() {
+			if(emailValidator == null) {
+				emailValidator = new ERXEmailValidator(true, true);
+			}
+			return emailValidator;
+		}
+		
+		public ERCMailAddress addressForEmailString(EOEditingContext ec, String email) {
+			ERCMailAddress result = objectMatchingKeyAndValue(ec, EMAIL_ADDRESS_KEY, email);
+			if(result == null) {
+				result = createAndInsertObject(ec);
+				result.setEmailAddress(email);
+			}
+			return result;
+		}
 	}
 
 	/**
@@ -29,6 +51,14 @@ public class ERCMailAddress extends er.corebl.model.eogen._ERCMailAddress {
 	 */
 	public void init(EOEditingContext ec) {
 		super.init(ec);
+		setIsActive(Boolean.TRUE);
 	}
 
+	public String validateEmailAddress(String value) {
+		if(!clazz.emailValidator().isValidEmailAddress(value, 100, true)) {
+			ERXValidationFactory factory = ERXValidationFactory.defaultFactory();
+			throw factory.createException(this, EMAIL_ADDRESS_KEY, value, ERXValidationException.InvalidValueException);
+		}
+		return value;
+	}
 }
