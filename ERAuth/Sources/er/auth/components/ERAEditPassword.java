d4ef59efb9e2662ff545cd433792dabb652722be
nullterminated <ramseygurley@gmail.com>
2012-05-28 12:51:29 -0700

    Fix serialization bug in ERAEditPassword

--- ERAuth/Sources/er/auth/components/ERAEditPassword.java
+++ ERAuth/Sources/er/auth/components/ERAEditPassword.java
@@ -16,6 +16,8 @@ import er.auth.processing.ERTwoFactorAuthenticationConfig;
 import er.directtoweb.components.ERDCustomComponent;
 import er.extensions.ERXExtensions;
 import er.extensions.appserver.ERXWOContext;
+import er.extensions.foundation.ERXEOSerializationUtilities;
+import er.extensions.foundation.ERXEOSerializationUtilities.SerialEOWrapper;
 import er.extensions.foundation.ERXSelectorUtilities;
 import er.extensions.validation.ERXValidationException;
 import er.extensions.validation.ERXValidationFactory;
@@ -26,7 +28,7 @@ public class ERAEditPassword extends ERDCustomComponent {
 	 * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
 	 * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
 
 	private static final Logger log = Logger.getLogger(ERAEditPassword.class);
 
@@ -37,7 +39,7 @@ public class ERAEditPassword extends ERDCustomComponent {
 	private String oldID;
 	private String newID;
 	private String verifyID;
-	private EOEnterpriseObject object;
+	private SerialEOWrapper object;
 
 	public ERAEditPassword(WOContext context) {
 		super(context);
@@ -184,11 +186,11 @@ public class ERAEditPassword extends ERDCustomComponent {
 	}
 
 	public EOEnterpriseObject object() {
-		return object;
+		return object.eo();
 	}
 
 	public void setObject(EOEnterpriseObject object) {
-		this.object = object;
+		this.object = new ERXEOSerializationUtilities.SerialEOWrapper(object);
 	}
 
 	/**
