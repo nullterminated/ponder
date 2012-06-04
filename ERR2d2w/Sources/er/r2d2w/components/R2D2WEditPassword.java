feaec7b0e281abb2377ade16d4013f9a3fecf0fd
nullterminated <ramseygurley@gmail.com>
2012-06-03 20:38:52 -0700

    Made edit password component stateless.

--- ERR2d2w/Sources/er/r2d2w/components/R2D2WEditPassword.java
+++ ERR2d2w/Sources/er/r2d2w/components/R2D2WEditPassword.java
@@ -1,65 +1,73 @@
 package er.r2d2w.components;
 
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+
 import com.webobjects.appserver.WOContext;
 import com.webobjects.appserver.WORequest;
 import com.webobjects.foundation.NSValidation;
 
-import er.directtoweb.components.ERDCustomEditComponent;
-import er.extensions.foundation.ERXStringUtilities;
+import er.directtoweb.components.ERD2WStatelessComponent;
+import er.extensions.appserver.ERXWOContext;
 import er.extensions.localization.ERXLocalizer;
+import er.extensions.validation.ERXValidationException;
 import er.extensions.validation.ERXValidationFactory;
 
-public class R2D2WEditPassword extends ERDCustomEditComponent {
+public class R2D2WEditPassword extends ERD2WStatelessComponent {
 	/**
-	 * Do I need to update serialVersionUID?
-	 * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
-	 * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
+	 * Do I need to update serialVersionUID? See section 5.6 <cite>Type Changes
+	 * Affecting Serialization</cite> on page 51 of the <a
+	 * href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object
+	 * Serialization Spec</a>
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 2L;
 
-    public R2D2WEditPassword(WOContext context) {
-        super(context);
-    }
-    
-    private static final String _COMPONENT_CLASS = "password";
+	private static final Logger log = Logger.getLogger(R2D2WEditPassword.class);
+
+	private static final String _COMPONENT_CLASS = "password";
 	private String labelID;
 	private String confirmLabelID;
-	private String confirmPassword;
 	private String password;
-	private boolean confirmSetToNull = false;
-	private boolean setToNull = false;
-    
+	private String confirmPassword;
+
+	public R2D2WEditPassword(WOContext context) {
+		super(context);
+	}
+
+	public void reset() {
+		super.reset();
+		labelID = null;
+		confirmLabelID = null;
+		confirmPassword = null;
+		password = null;
+	}
+
 	public String r2ConfirmLabelID() {
-		if(confirmLabelID == null) {
-			confirmLabelID = ERXStringUtilities.safeIdentifierName(context().elementID(), "id", '_');
+		if (confirmLabelID == null) {
+			confirmLabelID = ERXWOContext.safeIdentifierName(context(), false);
 		}
 		return confirmLabelID;
 	}
 
 	public String passwordLabelID() {
-		if(labelID == null) {
-			labelID = ERXStringUtilities.safeIdentifierName(context().elementID(), "id", '_');
+		if (labelID == null) {
+			labelID = ERXWOContext.safeIdentifierName(context(), false);
 		}
 		return labelID;
 	}
-	
+
 	public String componentClasses() {
-    	return _COMPONENT_CLASS;
-    }
-    
-	/**
-	 * @return the confirmPassword
-	 */
-	public String confirmPassword() {		
-		if(confirmSetToNull) {return null;}
-		return (confirmPassword == null)?(String)objectPropertyValue():confirmPassword;
+		return _COMPONENT_CLASS;
 	}
 
 	/**
-	 * @param confirmPassword the confirmPassword to set
+	 * @return the confirmPassword
 	 */
+	public String confirmPassword() {
+		return confirmPassword;
+	}
+	
 	public void setConfirmPassword(String confirmPassword) {
-		if(confirmPassword == null || confirmPassword.length() == 0) {confirmSetToNull = true;}
 		this.confirmPassword = confirmPassword;
 	}
 
@@ -67,69 +75,50 @@ public class R2D2WEditPassword extends ERDCustomEditComponent {
 	 * @return the password
 	 */
 	public String password() {
-		if(setToNull) {return null;}
-		return (password == null)?(String)objectPropertyValue():password;
+		return password;
 	}
-
-	/**
-	 * @param password the password to set
-	 */
+	
 	public void setPassword(String password) {
-		if(password == null || password.length() == 0) {setToNull = true;}
 		this.password = password;
 	}
 
-    public void reset() {
-    	super.reset();
-    	confirmLabelID = null;
-    	confirmPassword = null;
-    	confirmSetToNull = false;
-    	labelID = null;
-    	password = null;
-    	setToNull = false;
-    }
+	public void fail(String errorCode) {
+		if (log.isDebugEnabled()) {
+			log.debug("fail:<object:" + object() + "; key:" + propertyKey() + "; code:" + errorCode + ";>");
+		}
+		ERXValidationFactory factory = ERXValidationFactory.defaultFactory();
+		ERXValidationException ex = factory.createException(object(), propertyKey(), null, errorCode);
+		validationFailedWithException(ex, null, propertyKey());
+	}
 
-    public void fail(String errorCode) {
-        if(log.isDebugEnabled()) {
-            log.debug("fail:<object:" + object() + "; key:" + key() + ";  password: " + password() + "; code:" + errorCode + ";>");
-        }
-        validationFailedWithException(ERXValidationFactory.defaultFactory().createException(object(), key(), password(), errorCode), password(), key());
-    }
+	protected void checkPasswords() {
+		if (StringUtils.isBlank(password()) || StringUtils.isBlank(confirmPassword())) {
+			fail("PasswordsFillBothFieldsException");
+		} else if (!password().equals(confirmPassword())) {
+			fail("PasswordsDontMatchException");
+		} else {
+			try {
+				object().validateTakeValueForKeyPath(password(), propertyKey());
+			} catch (NSValidation.ValidationException ex) {
+				validationFailedWithException(ex, null, propertyKey());
+			}
+		}
+	}
 
-    //TODO figure out how to do this in a cleaner way?
-    protected void checkPasswords() {
-    	if(password() == null || password().length() == 0 || confirmPassword() == null || confirmPassword().length() == 0) {
-    		confirmPassword = (String)objectPropertyValue();
-    		password = (String)objectPropertyValue();
-    		confirmSetToNull = false;
-    		setToNull = false;
-    		fail("PasswordsFillBothFieldsException");
-    	} else if (!password().equals(confirmPassword())) {
-    		confirmPassword = (String)objectPropertyValue();
-    		password = (String)objectPropertyValue();
-    		confirmSetToNull = false;
-    		setToNull = false;
-    		fail("PasswordsDontMatchException");    		
-    	} else {
-            try {
-    			object().validateTakeValueForKeyPath(password(), key());
-    		} catch(NSValidation.ValidationException ex) {
-        		confirmPassword = (String)objectPropertyValue();
-        		password = (String)objectPropertyValue();
-        		confirmSetToNull = false;
-        		setToNull = false;
-    			validationFailedWithException(ex, password(), key());
-    		}
-    	}
-    }
-    
-    public void takeValuesFromRequest(WORequest r, WOContext c) {
-        super.takeValuesFromRequest(r,c);
-        checkPasswords();
+	public void takeValuesFromRequest(WORequest r, WOContext c) {
+		super.takeValuesFromRequest(r, c);
+		checkPasswords();
 	}
 
 	public String confirmToolTip() {
-		return ERXLocalizer.currentLocalizer().localizedTemplateStringForKeyWithObject("R2D2WEditPassword.confirmToolTip", d2wContext());
+		return ERXLocalizer.currentLocalizer().localizedTemplateStringForKeyWithObject(
+				"R2D2WEditPassword.confirmToolTip", d2wContext());
 	}
 
+	/**
+	 * Validation Support. Passes errors to the parent.
+	 */
+	public void validationFailedWithException(Throwable e, Object value, String keyPath) {
+		parent().validationFailedWithException(e, value, keyPath);
+	}
 }
