4e8a2e86ef2dae7e4d8907c2d2de3addd2efa904
nullterminated <ramseygurley@gmail.com>
2012-06-10 19:11:01 -0700

    New property level message component

---
+++ ERR2d2w/Sources/er/r2d2w/components/R2D2WPropertyMessage.java
@@ -0,0 +1,10 @@
+package er.r2d2w.components;
+
+import com.webobjects.appserver.WOContext;
+import er.directtoweb.components.ERD2WStatelessComponent;
+
+public class R2D2WPropertyMessage extends ERD2WStatelessComponent {
+    public R2D2WPropertyMessage(WOContext context) {
+        super(context);
+    }
+}
