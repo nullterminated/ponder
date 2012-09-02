a81df239384db1c50019aa030d1df417e240eb2b
nullterminated <ramseygurley@gmail.com>
2012-09-02 09:59:36 -0700

    Created crud auth delegate for allow all in addition to the existing deny all.

--- ERAuth/Sources/er/auth/SimpleCRUDAuthorization.java
+++ ERAuth/Sources/er/auth/SimpleCRUDAuthorization.java
@@ -5,53 +5,61 @@ import com.webobjects.eocontrol.EOQualifier;
 
 import er.extensions.eof.EOEnterpriseObjectClazz;
 import er.extensions.qualifiers.ERXFalseQualifier;
+import er.extensions.qualifiers.ERXTrueQualifier;
 
 /**
- * The default CRUD authorization delegate. It allows no access to anything.
+ * Simple CRUD authorization delegates. ALLOW_ALL gives all access, DENY_ALL rejects all access.
  */
-public enum DefaultCRUDAuthorization implements CRUDAuthorization {
-	INSTANCE;
+public enum SimpleCRUDAuthorization implements CRUDAuthorization {
+	ALLOW_ALL(Boolean.TRUE, new ERXTrueQualifier()), 
+	DENY_ALL(Boolean.FALSE, new ERXFalseQualifier());
 	
-	private static final EOQualifier QUALIFIER = new ERXFalseQualifier();
+	private final EOQualifier _qualifier;
+	private final Boolean _allow;
 
+	SimpleCRUDAuthorization(Boolean allow, EOQualifier qualifier) {
+		_allow = allow;
+		_qualifier = qualifier;
+	}
+	
 	@Override
 	public <T extends EOEnterpriseObject> Boolean canCreate(EOEnterpriseObjectClazz<T> clazz) {
-		return Boolean.FALSE;
+		return _allow;
 	}
 
 	@Override
 	public Boolean canDelete(EOEnterpriseObject eo) {
-		return Boolean.FALSE;
+		return _allow;
 	}
 
 	@Override
 	public <T extends EOEnterpriseObject> Boolean canQuery(EOEnterpriseObjectClazz<T> clazz) {
-		return Boolean.FALSE;
+		return _allow;
 	}
 
 	@Override
 	public Boolean canRead(EOEnterpriseObject eo) {
-		return Boolean.FALSE;
+		return _allow;
 	}
 
 	@Override
 	public Boolean canReadProperty(EOEnterpriseObject eo, String keyPath) {
-		return Boolean.FALSE;
+		return _allow;
 	}
 
 	@Override
 	public Boolean canUpdate(EOEnterpriseObject eo) {
-		return Boolean.FALSE;
+		return _allow;
 	}
 
 	@Override
 	public Boolean canUpdateProperty(EOEnterpriseObject eo, String keyPath) {
-		return Boolean.FALSE;
+		return _allow;
 	}
 
 	@Override
 	public <T extends EOEnterpriseObject> EOQualifier restrictingQueryQualifier(EOEnterpriseObjectClazz<T> clazz) {
-		return QUALIFIER;
+		return _qualifier;
 	}
 
 }
