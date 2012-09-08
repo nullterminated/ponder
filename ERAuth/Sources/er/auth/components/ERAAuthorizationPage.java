b516c3b64c12f8f38220b01e1bd436664bd59728
nullterminated <ramseygurley@gmail.com>
2012-09-08 10:38:58 -0700

    New d2w page for editing role based authorizations

---
+++ ERAuth/Sources/er/auth/components/ERAAuthorizationPage.java
@@ -0,0 +1,142 @@
+package er.auth.components;
+
+import com.webobjects.appserver.WOActionResults;
+import com.webobjects.appserver.WOContext;
+import com.webobjects.eocontrol.EOEditingContext;
+import com.webobjects.eocontrol.EOQualifier;
+import com.webobjects.foundation.NSArray;
+import com.webobjects.foundation.NSMutableArray;
+
+import er.auth.model.EREntityPermission;
+import er.auth.model.ERPropertyPermission;
+import er.auth.model.ERRole;
+import er.directtoweb.pages.ERD2WPage;
+import er.extensions.eof.ERXEC;
+import er.extensions.eof.ERXFetchSpecification;
+import er.extensions.eof.ERXS;
+import er.extensions.eof.ERXSortOrdering.ERXSortOrderings;
+import er.extensions.foundation.ERXArrayUtilities;
+
+public class ERAAuthorizationPage extends ERD2WPage {
+	/**
+	 * Do I need to update serialVersionUID?
+	 * See section 5.6 <cite>Type Changes Affecting Serialization</cite> on page 51 of the 
+	 * <a href="http://java.sun.com/j2se/1.4/pdf/serial-spec.pdf">Java Object Serialization Spec</a>
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private NSArray<ERRole> _roles;
+
+	private EREntityPermission _selectedPermission;
+
+	private NSArray<EREntityPermission> _entityPermissions;
+
+	private NSArray<ERPropertyPermission> _propertyPermissions;
+
+	public ERAAuthorizationPage(WOContext context) {
+        super(context);
+    }
+	
+	@Override
+	public EOEditingContext editingContext() {
+		if(_context == null) {
+			_context = ERXEC.newEditingContext();
+			_context.lock();
+		}
+		return _context;
+	}
+	
+	public NSArray<ERRole> roles() {
+		if(_roles == null) {
+			_roles = EREntityPermission.ROLE.arrayValueInObject(entityPermissions());
+		}
+		return _roles;
+	}
+
+	/**
+	 * @return the selectedPermission
+	 */
+	public EREntityPermission selectedPermission() {
+		return _selectedPermission;
+	}
+
+	/**
+	 * @param selectedPermission the selectedPermission to set
+	 */
+	public void setSelectedPermission(EREntityPermission selectedPermission) {
+		this._selectedPermission = selectedPermission;
+	}
+
+	/**
+	 * @return the entityPermissions
+	 */
+	public NSArray<EREntityPermission> entityPermissions() {
+		if(_entityPermissions == null) {
+			String entityName = d2wContext().entity().name();
+			NSArray<String> roleNames = (NSArray<String>) d2wContext().valueForKey("authorizeRoleNames");
+			EOQualifier q = EREntityPermission.ROLE.dot(ERRole.ROLE_NAME).in(roleNames)
+					.and(EREntityPermission.NAME_FOR_ENTITY.eq(entityName));
+			ERXSortOrderings so = EREntityPermission.ROLE.dot(ERRole.ROLE_NAME).ascs();
+			ERXFetchSpecification<EREntityPermission> fs = 
+					new ERXFetchSpecification<EREntityPermission>(EREntityPermission.ENTITY_NAME, q, so);
+			NSArray<String> prefetch = new NSArray<String>(EREntityPermission.ROLE_KEY);
+			fs.setPrefetchingRelationshipKeyPaths(prefetch);
+			NSArray<EREntityPermission> permissions = fs.fetchObjects(editingContext());
+			int diff = roles().count() - permissions.count();
+			if(diff > 0) {
+				NSMutableArray<EREntityPermission> newPermissions = 
+						new NSMutableArray<EREntityPermission>(diff);
+				NSArray<ERRole> permRoles = EREntityPermission.ROLE.arrayValueInObject(permissions);
+				NSArray<ERRole> roles = ERXArrayUtilities.arrayMinusArray(roles(), permRoles);
+				for(ERRole role: roles) {
+					EREntityPermission permission = role.createEntityPermissionsRelationship();
+					permission.setNameForEntity(entityName);
+					newPermissions.addObject(permission);
+				}
+				permissions = permissions.arrayByAddingObjectsFromArray(newPermissions);
+				permissions = ERXS.sorted(permissions, so);
+			}
+			_entityPermissions = permissions;
+		}
+		return _entityPermissions;
+	}
+
+	public WOActionResults selectedPermissionAction() {
+		_propertyPermissions = null;
+		return null;
+	}
+
+	/**
+	 * @return the propertyPermissions
+	 */
+	public NSArray<ERPropertyPermission> propertyPermissions() {
+		if(_propertyPermissions == null && selectedPermission() != null) {
+			NSArray<String> propertyKeys = (NSArray<String>) d2wContext().valueForKey("authorizePropertyKeys");
+			EOQualifier q = ERPropertyPermission.ENTITY_PERMISSION.eq(selectedPermission())
+					.and(ERPropertyPermission.NAME_FOR_PROPERTY.in(propertyKeys));
+			ERXSortOrderings so = ERPropertyPermission.NAME_FOR_PROPERTY.ascs();
+			ERXFetchSpecification<ERPropertyPermission> fs = 
+					new ERXFetchSpecification<ERPropertyPermission>(ERPropertyPermission.ENTITY_NAME, q, so);
+			fs.setIncludeEditingContextChanges(true);
+			NSArray<ERPropertyPermission> permissions = fs.fetchObjects(editingContext());
+			int diff = propertyKeys.count() - permissions.count();
+			if(diff > 0) {
+				NSMutableArray<ERPropertyPermission> newPermissions =
+						new NSMutableArray<ERPropertyPermission>(diff);
+				NSArray<String> permProps = 
+						ERPropertyPermission.NAME_FOR_PROPERTY.arrayValueInObject(permissions);
+				NSArray<String> newProps = ERXArrayUtilities.arrayMinusArray(propertyKeys, permProps);
+				for(String propertyKey : newProps) {
+					ERPropertyPermission permission = 
+							selectedPermission().createPropertyPermissionsRelationship();
+					permission.setNameForProperty(propertyKey);
+					newPermissions.addObject(permission);
+				}
+				permissions = permissions.arrayByAddingObjectsFromArray(newPermissions);
+				permissions = ERXS.sorted(permissions, so);
+			}
+			_propertyPermissions = permissions;
+		}
+		return _propertyPermissions;
+	}
+}
