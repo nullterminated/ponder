03a178b953ef8efb8fe275058863be1669cf4a2a
nullterminated <ramseygurley@gmail.com>
2012-12-19 08:35:15 -0700

    Add a static delegate to allow customized request handling.

--- AWSPlugin/Sources/er/awsplugin/handlers/SESRequestHandler.java
+++ AWSPlugin/Sources/er/awsplugin/handlers/SESRequestHandler.java
@@ -11,12 +11,32 @@ import er.corebl.mail.ERCMailer;
 
 public class SESRequestHandler implements RequestHandler {
 	private static final Logger log = Logger.getLogger(SESRequestHandler.class);
+	
+	private static Delegate _delegate;
+
+	public static interface Delegate {
+		public void afterError(Request<?> request, Exception exception);
+		public void afterResponse(Request<?> request, Object obj, TimingInfo timinginfo);
+		public void beforeRequest(Request<?> request);
+	}
+	
+	public static Delegate delegate() {
+		return _delegate;
+	}
+
+	public static void setDelegate(Delegate delegate) {
+		_delegate = delegate;
+	}
 
 	@Override
 	public void afterError(Request<?> request, Exception exception) {
 		// Does nothing
-		log.info("Request: " + request);
-		log.info("Error: " + exception.toString(), exception);
+		if(_delegate != null) {
+			_delegate.afterError(request, exception);
+		} else {
+			log.info("Request: " + request);
+			log.info("Error: " + exception.toString(), exception);
+		}
 	}
 
 	/**
@@ -24,15 +44,23 @@ public class SESRequestHandler implements RequestHandler {
 	 */
 	@Override
 	public void afterResponse(Request<?> request, Object obj, TimingInfo timinginfo) {
-		SendRawEmailResult result = (SendRawEmailResult) obj;
-		String messageID = result.getMessageId();
-		ERCMailer.INSTANCE.setMessageID(messageID);
+		if(_delegate != null) {
+			_delegate.afterResponse(request, obj, timinginfo);
+		} else {
+			SendRawEmailResult result = (SendRawEmailResult) obj;
+			String messageID = result.getMessageId();
+			ERCMailer.INSTANCE.setMessageID(messageID);
+		}
 	}
 
 	@Override
 	public void beforeRequest(Request<?> request) {
 		// Does nothing
-		log.info("Before request");
+		if(_delegate != null) {
+			_delegate.beforeRequest(request);
+		} else {
+			log.info("Before request");
+		}
 	}
 
 }
