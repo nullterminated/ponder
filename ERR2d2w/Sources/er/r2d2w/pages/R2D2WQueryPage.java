f563444c1d5ad44bb045905180610e5be0e8632b
nullterminated <ramseygurley@gmail.com>
2013-01-06 22:07:12 -0700

    update query page to use the hasForm key

--- ERR2d2w/Sources/er/r2d2w/pages/R2D2WQueryPage.java
+++ ERR2d2w/Sources/er/r2d2w/pages/R2D2WQueryPage.java
@@ -3,6 +3,7 @@ package er.r2d2w.pages;
 import com.webobjects.appserver.WOContext;
 
 import er.directtoweb.pages.ERD2WQueryPage;
+import er.extensions.foundation.ERXValueUtilities;
 
 public class R2D2WQueryPage extends ERD2WQueryPage {
 	/**
@@ -16,4 +17,8 @@ public class R2D2WQueryPage extends ERD2WQueryPage {
         super(context);
     }
 
+	public boolean disableForm() {
+		boolean hasForm = ERXValueUtilities.booleanValue(d2wContext().valueForKey("hasForm"));
+		return context().isInForm() || !hasForm;
+	}
 }
