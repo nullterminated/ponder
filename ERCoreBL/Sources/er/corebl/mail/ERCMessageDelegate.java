9f4ce849a8d3b2ffc2de7802bb71cb068af93fe6
nullterminated <ramseygurley@gmail.com>
2013-01-08 12:47:42 -0700

    Set the dateLastSent on the mail address after sending.

--- ERCoreBL/Sources/er/corebl/mail/ERCMessageDelegate.java
+++ ERCoreBL/Sources/er/corebl/mail/ERCMessageDelegate.java
@@ -7,7 +7,9 @@ import com.webobjects.eocontrol.EOKeyGlobalID;
 import com.webobjects.foundation.NSArray;
 import com.webobjects.foundation.NSTimestamp;
 
+import er.corebl.model.ERCMailAddress;
 import er.corebl.model.ERCMailMessage;
+import er.corebl.model.ERCMailRecipient;
 import er.extensions.eof.ERXEC;
 import er.javamail.ERMessage;
 import er.javamail.ERMessage.Delegate;
@@ -26,7 +28,9 @@ public class ERCMessageDelegate implements Delegate {
 		EOEditingContext ec = ERXEC.newEditingContext();
 		ERCMailMessage mailMessage = (ERCMailMessage) ec.faultForGlobalID(_gid, ec);
 		mailMessage.setState(ERCMailState.SENT);
-		mailMessage.setDateSent(new NSTimestamp());
+		NSTimestamp now = new NSTimestamp();
+		mailMessage.setDateSent(now);
+		ERCMailMessage.MAIL_RECIPIENTS.dot(ERCMailRecipient.MAIL_ADDRESS).dot(ERCMailAddress.DATE_LAST_SENT).takeValueInObject(now, mailMessage);
 		ec.saveChanges();
 	}
 
