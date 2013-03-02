a027b24638ee4e05ad738efbf20d89bc5050cf86
nullterminated <ramseygurley@gmail.com>
2013-03-02 11:14:07 -0700

    Create dynamic relationships after models are loaded.

--- AWSPlugin/Sources/er/awsplugin/AWSPlugin.java
+++ AWSPlugin/Sources/er/awsplugin/AWSPlugin.java
@@ -2,19 +2,25 @@ package er.awsplugin;
 
 import org.apache.log4j.Logger;
 
+import com.webobjects.eoaccess.EOModelGroup;
 import com.webobjects.eocontrol.EOClassDescription;
+import com.webobjects.foundation.NSNotification;
+import com.webobjects.foundation.NSNotificationCenter;
 
 import er.awsplugin.model.SESBounceNotification;
 import er.awsplugin.model.SESComplaintNotification;
 import er.corebl.ERCoreBL;
 import er.corebl.model.ERCMailAddress;
+import er.extensions.ERXExtensions;
 import er.extensions.ERXFrameworkPrincipal;
 import er.extensions.eof.ERXEOAccessUtilities;
+import er.extensions.eof.ERXModelGroup;
+import er.extensions.foundation.ERXSelectorUtilities;
 
 public class AWSPlugin extends ERXFrameworkPrincipal {
 
     public static final Logger log = Logger.getLogger(AWSPlugin.class);
-    public final static Class<?>[] REQUIRES = new Class[] {ERCoreBL.class};
+    public final static Class<?>[] REQUIRES = new Class[] {ERXExtensions.class, ERCoreBL.class};
 
     protected static volatile AWSPlugin sharedInstance;
 
@@ -41,7 +47,18 @@ public class AWSPlugin extends ERXFrameworkPrincipal {
     	 * Dynamically create reverse relationships for notifications
     	 * to prevent tying ERCoreBL to AWSPlugin
     	 */
-    	ERXEOAccessUtilities.createRelationship("complaints", ERCMailAddress.ENTITY_NAME, "id", SESComplaintNotification.ENTITY_NAME, "mailAddressID", true, EOClassDescription.DeleteRuleDeny, false, true, false);
-    	ERXEOAccessUtilities.createRelationship("bounces", ERCMailAddress.ENTITY_NAME, "id", SESBounceNotification.ENTITY_NAME, "mailAddressID", true, EOClassDescription.DeleteRuleDeny, false, true, false);
+    	NSNotificationCenter.defaultCenter().addObserver(
+    			this, 
+    			ERXSelectorUtilities.notificationSelector("modelGroupAdded"), 
+    			ERXModelGroup.ModelGroupAddedNotification, 
+    			null);
+    }
+    
+    public void modelGroupAdded(NSNotification n) {
+    	EOModelGroup group = (EOModelGroup) n.object();
+    	if(group.modelNamed("ERCoreBL") != null && group.modelNamed("AWSPlugin") != null) {
+        	ERXEOAccessUtilities.createRelationship("complaints", ERCMailAddress.ENTITY_NAME, "id", SESComplaintNotification.ENTITY_NAME, "mailAddressID", true, EOClassDescription.DeleteRuleDeny, false, true, false);
+        	ERXEOAccessUtilities.createRelationship("bounces", ERCMailAddress.ENTITY_NAME, "id", SESBounceNotification.ENTITY_NAME, "mailAddressID", true, EOClassDescription.DeleteRuleDeny, false, true, false);
+    	}
     }
 }
