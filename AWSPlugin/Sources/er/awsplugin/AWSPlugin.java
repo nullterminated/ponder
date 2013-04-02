de8ae41f25eb72505138e9dfdeef3e8a6445fe3c
nullterminated <ramseygurley@gmail.com>
2013-04-01 20:40:40 -0700

    Removing duplicate code now that fixes are available in wonder master.

--- AWSPlugin/Sources/er/awsplugin/AWSPlugin.java
+++ AWSPlugin/Sources/er/awsplugin/AWSPlugin.java
@@ -2,14 +2,8 @@ package er.awsplugin;
 
 import org.apache.log4j.Logger;
 
-import com.webobjects.eoaccess.EOAttribute;
-import com.webobjects.eoaccess.EOEntity;
-import com.webobjects.eoaccess.EOJoin;
 import com.webobjects.eoaccess.EOModelGroup;
-import com.webobjects.eoaccess.EOProperty;
-import com.webobjects.eoaccess.EORelationship;
 import com.webobjects.eocontrol.EOClassDescription;
-import com.webobjects.foundation.NSMutableArray;
 import com.webobjects.foundation.NSNotification;
 import com.webobjects.foundation.NSNotificationCenter;
 
@@ -19,6 +13,7 @@ import er.corebl.ERCoreBL;
 import er.corebl.model.ERCMailAddress;
 import er.extensions.ERXExtensions;
 import er.extensions.ERXFrameworkPrincipal;
+import er.extensions.eof.ERXEOAccessUtilities;
 import er.extensions.eof.ERXModelGroup;
 import er.extensions.foundation.ERXSelectorUtilities;
 
@@ -62,63 +57,8 @@ public class AWSPlugin extends ERXFrameworkPrincipal {
     public void modelGroupAdded(NSNotification n) {
     	EOModelGroup group = (EOModelGroup) n.object();
     	if(group.modelNamed("ERCoreBL") != null && group.modelNamed("AWSPlugin") != null) {
-        	createRelationship("complaints", ERCMailAddress.ENTITY_NAME, "id", SESComplaintNotification.ENTITY_NAME, "mailAddressID", true, EOClassDescription.DeleteRuleDeny, false, true, false);
-        	createRelationship("bounces", ERCMailAddress.ENTITY_NAME, "id", SESBounceNotification.ENTITY_NAME, "mailAddressID", true, EOClassDescription.DeleteRuleDeny, false, true, false);
+        	ERXEOAccessUtilities.createRelationship("complaints", ERCMailAddress.ENTITY_NAME, "id", SESComplaintNotification.ENTITY_NAME, "mailAddressID", true, EOClassDescription.DeleteRuleDeny, false, true, false);
+        	ERXEOAccessUtilities.createRelationship("bounces", ERCMailAddress.ENTITY_NAME, "id", SESBounceNotification.ENTITY_NAME, "mailAddressID", true, EOClassDescription.DeleteRuleDeny, false, true, false);
     	}
     }
-    
-    //TODO remove this method and use the one in ERXEOAccessUtilities when the fix hits master.
-	/**
-	 * Programatically adds a toOne or toMany relationship to an entity at runtime.
-	 * 
-	 * @param relationshipName name of the relationship to be created on the source entity
-	 * @param sourceEntityName name of the source entity
-	 * @param sourceAttributeName name of the attribute in the source entity to be used by the join
-	 * @param destinationEntityName name of the destination entity
-	 * @param destinationAttributeName name of the attribute in the destination entity to be used by the join
-	 * @param toMany if true, the relationship will be toMany, otherwise it will be toOne
-	 * @param deleteRule
-	 *            EOClassDescription.DeleteRuleCascade ||
-	 *            EOClassDescription.DeleteRuleDeny ||
-	 *            EOClassDescription.DeleteRuleNoAction ||
-	 *            EOClassDescription.DeleteRuleNullify
-	 * @param isMandatory mandatory or not
-	 * @param isClassProperty class property or not
-	 * @param shouldPropagatePrimaryKey propagate primary key or not
-	 * @return the newly created relationship
-	 * 
-	 * @author th
-	 */
-	private static EORelationship createRelationship(String relationshipName, String sourceEntityName, String sourceAttributeName, String destinationEntityName, String destinationAttributeName, boolean toMany, int deleteRule, boolean isMandatory, boolean isClassProperty, boolean shouldPropagatePrimaryKey) {
-		EOEntity sourceEntity = EOModelGroup.defaultGroup().entityNamed(sourceEntityName);
-		if(sourceEntity.isAbstractEntity())
-			log.warn("If you programatically add relationships to an abstract entity, make sure you also add it to child entities");
-		EOEntity destinationEntity = EOModelGroup.defaultGroup().entityNamed(destinationEntityName);
-		EOAttribute sourceAttribute = sourceEntity.attributeNamed(sourceAttributeName);
-		EOAttribute destinationAttribute = destinationEntity.attributeNamed(destinationAttributeName);
-		EOJoin join = new EOJoin(sourceAttribute, destinationAttribute);
-		EORelationship relationship = new EORelationship();
-
-		relationship.setName(relationshipName);
-		sourceEntity.addRelationship(relationship);
-		relationship.addJoin(join);
-		relationship.setToMany(toMany);
-		relationship.setJoinSemantic(EORelationship.InnerJoin);
-		relationship.setDeleteRule(deleteRule);
-		relationship.setIsMandatory(isMandatory);
-		relationship.setPropagatesPrimaryKey(shouldPropagatePrimaryKey);
-		NSMutableArray<EOProperty> classProperties = sourceEntity.classProperties().mutableClone();
-		if (isClassProperty && !classProperties.containsObject(relationship)) {
-			classProperties.addObject(relationship);
-			sourceEntity.setClassProperties(classProperties);
-		} else if (!isClassProperty && classProperties.containsObject(relationship)) {
-			classProperties.removeObject(relationship);
-			sourceEntity.setClassProperties(classProperties);
-		}
-		if(log.isDebugEnabled())
-			log.debug(relationship);
-
-		return relationship;
-	}
-
 }
